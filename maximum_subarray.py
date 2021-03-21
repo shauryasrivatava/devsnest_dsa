4d2aef6b7748d82413d3cc6c0319cdfc605a4270
Shaurya Srivastava <64094563+shauryasrivatava@users.noreply.github.com>
2021-03-22 00:02:53 +0530

    Update maximum_subarray.py

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -1,11 +1,12 @@
 class Solution(object):
     def maxSubArray(self, nums):
-        max_sum=max(nums)
-        if max_sum<0:
-            return max_sum;
+        max_num=max(nums)
+        if max_num<0:
+            return max_num;
         curr_sum=0
+        max_sum=0
         for i in nums:
-            curr_sum+=nums[i]
+            curr_sum += i
             if max_sum<curr_sum:
                 max_sum=curr_sum
             if curr_sum<0:
