6a0a4a06381c4e8fbe66907adb66156a5853eac7
Shaurya Srivastava <64094563+shauryasrivatava@users.noreply.github.com>
2021-04-07 01:32:48 +0530

    Update 69_Sqrt(x).py

--- 69_Sqrt(x).py
+++ 69_Sqrt(x).py
@@ -1,11 +1,26 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        # return int(math.sqrt(x))
+#         return int(math.sqrt(x))
         
-        ans=1
-        while (ans*ans)<x:
-            ans+=1
-        if ans*ans>x:
-            ans-=1
-        return ans
-        
+#         ans=1
+#         while (ans*ans)<x:
+#             ans+=1
+#         if ans*ans>x:
+#             ans-=1
+#         return ans
+
+#         In most efficient Time complexity
+        lb=0
+        ub=x
+        mid=0
+        if x==1:
+            return 1
+        while lb<=ub:
+            mid=(lb+ub)//2
+            if mid*mid==x:
+                return mid
+            if mid*mid<x:
+                lb=mid+1
+            else:
+                ub=mid-1
+        return mid   
